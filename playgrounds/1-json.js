b624a0d5580a6a55b9c2692629cd95c195134633
Anvar pk <anvar.pk@quest-global.com>
2021-10-22 14:22:45 +0530

    Excercise: Work with JSON and the file system

--- playgrounds/1-json.js
+++ playgrounds/1-json.js
@@ -9,6 +9,10 @@ const fs = require('fs');
 
 const dataBuffer = fs.readFileSync('i-json.json');
 const dataJson = dataBuffer.toString();
-const data  = JSON.parse(dataJson);
+const data = JSON.parse(dataJson);
+data.name = "Anvar";
+data.planet = "Mercury";
+data.age = 28;
 
-console.log(data.title)
+const saveData = JSON.stringify(data);
+fs.writeFileSync('i-json.json', saveData);
