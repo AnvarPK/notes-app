54216603cb3dbfc3cae54b637179bcce9fc3ca44
Anvar pk <anvar.pk@quest-global.com>
2021-10-22 14:14:58 +0530

    ReadFileSync: dataBuffer

--- playgrounds/1-json.js
+++ playgrounds/1-json.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
-const book = {
-    title: 'Ego is Enemy',
-    author: 'Ryan'
-}
+// const book = {
+//     title: 'Ego is Enemy',
+//     author: 'Ryan'
+// }
 
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync('i-json.json', bookJSON);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('i-json.json', bookJSON);
+
+const dataBuffer = fs.readFileSync('i-json.json');
+const dataJson = dataBuffer.toString();
+const data  = JSON.parse(dataJson);
+
+console.log(data.title)
